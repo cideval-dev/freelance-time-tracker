11585895f8b73ff907c6276a6ca526303123f4fb
Cideval Games <83017594+cideval-dev@users.noreply.github.com>
2026-06-03 18:58:02 +0200

    feat: implement stopwatch logic with formatted time display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,42 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
-import { Play } from "lucide-react";
+import { Play, Square } from "lucide-react";
+import { useState, useEffect } from "react";
+
+function formatTime(time: number): string {
+  const hours: number = Math.floor(time / 3600);
+  const minutes: number = Math.floor(time % 3600 / 60);
+  const seconds: number = Math.floor(time % 3600 % 60);
+
+  return `${String(hours).padStart(2, '0')} : ${String(minutes).padStart(2, '0')} : ${String(seconds).padStart(2, '0')}`;
+}
 
 export default function Home() {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [time, setTime] = useState<number>(0);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    }
+  }, [isPlaying])
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-6">Mon Tracker de Temps</h1>
-      <Button><Play className="mr-2 size-4 fill-current" /> Démarrer</Button>
+      <h1 className="text-4xl font-bold mb-6">{formatTime(time)}</h1>
+      <Button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? <Square className="mr-2 size-4 fill-current" /> : <Play className="mr-2 size-4 fill-current" />} 
+        {isPlaying ? "Arrêter" : "Démarrer"}
+        </Button>
     </div>
   );
 }
